f8776540b5b7b792917597a40d73dc48f1c35cb0
iwanami-intwk <iwanami@intwk.co.jp>
2018-12-25 18:13:36 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var RED = require('node-red');
+var app = express();
+var http = require('http');
+
+const PORT = process.env.PORT||8000;
+
+var server = http.createServer(app);
+var settings = require("./settings.js");
+
+RED.init(server,settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+ 
+server.listen(settings.uiPort);
+
+RED.start();
+
